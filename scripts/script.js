07f2ab7f671f0ffa165cef31a30fe370e36ce3b9
Wen Xu <deslovexu@gmail.com>
2023-06-17 21:14:35 -0500

    portfolio updated

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+
+// scrolling behavior
+// Smooth scrolling animation for all anchor links
+$(document).ready(function(){
+    // Add smooth scrolling to all anchor links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+// collapsible sections
+/* const coll = document.getElementsByClassName("researchSection");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} */
